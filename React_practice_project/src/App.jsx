cd521a03b6d345a9307af7279fa834ac4ad406ba
Almuheetu Shihab <almuheetu@gmail.com>
2024-01-30 17:19:06 +0600

    conditional rendaring

--- React_practice_project/src/App.jsx
+++ React_practice_project/src/App.jsx
@@ -5,12 +5,76 @@ import Hero from "./component/Hero";
 
 
 const App = () => {
+    const city=["Dhaka","London","Delhi","Kolkata"]
   return (
     <div>
-      <Header/>
-      <Hero/>   
-      <ContactForm/>
-      <Footer/>
+
+
+
+
+
+
+
+
+
+    {/* <ol>
+      {
+        city.map((item,i)=>{
+
+          return <li key={i.toString()}>{item}</li>
+
+        })
+
+      }
+    </ol> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* // IMMEDIATELY_INVOKED FUNCTION */}
+    {/* {(()=>{
+
+      if(marks>=80 && marks<100){
+        return <h1>A+</h1>
+      }
+      else if(marks>=70 && marks<80){
+        return <h1>A</h1>
+      }
+      else if(marks>=60 && marks<70){
+        return <h1>A-</h1>
+      }
+      else if(marks>=50 && marks<60){
+        return <h1>B</h1>
+      }
+      else if(marks>=40 && marks<50){
+        return <h1>C</h1>
+      }
+      else{
+        return <h1>F</h1>
+      }
+       
+
+      })()} */}
+
+
+      {/* // Inline if else */}
+      {/* {  
+        marks>90?
+        <h1>Brilliant Result</h1>
+        :
+        <h1>Avarage Result</h1>
+      } */}
     </div>
   );
 };
