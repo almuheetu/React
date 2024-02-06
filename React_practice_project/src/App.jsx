26a63a3298f1f85311c254d84574dc5331d9e39f
Almuheetu Shihab <almuheetu@gmail.com>
2024-02-07 01:03:36 +0600

    // UseRef CSS class nie kaj kora

--- React_practice_project/src/App.jsx
+++ React_practice_project/src/App.jsx
@@ -3,16 +3,28 @@
 // import Header from "./component/Header";
 // import Hero from "./component/Hero";
 
+import { useRef } from "react";
 
-
+// UseRef CSS class nie kaj kora
 
 const App = () => {
 
+let firstName=useRef();
+let lastName=useRef();
 
-// UseRef Method
+const change = () => {
+  let fName= firstName.current.value;
+  let lName= lastName.current.value;
 
+  alert(fName +" "+lName);
+}
 return (
   <div>
+
+    <input ref={firstName} placeholder="First Name"/><br/>
+    <input ref={lastName} placeholder="Last Name"/><br/>
+
+    <button onClick={change}>Click</button>
 
   </div>
 );
