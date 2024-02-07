0f56132d1b4fe6a5d10b7148233f2bf3c8c9e82c
Almuheetu Shihab <almuheetu@gmail.com>
2024-02-07 17:51:51 +0600

    fixed

--- React_practice_project/src/App.jsx
+++ React_practice_project/src/App.jsx
@@ -3,26 +3,20 @@
 // import Header from "./component/Header";
 // import Hero from "./component/Hero";
 
-import { useRef } from "react";
 
 // UseRef CSS class nie kaj kora
 
 const App = () => {
 
-let firstName=useRef();
-let lastName=useRef();
+
 
 const change = () => {
-  let fName= firstName.current.value;
-  let lName= lastName.current.value;
 
-  alert(fName +" "+lName);
 }
 return (
   <div>
 
-    <input ref={firstName} placeholder="First Name"/><br/>
-    <input ref={lastName} placeholder="Last Name"/><br/>
+
 
     <button onClick={change}>Click</button>
 
